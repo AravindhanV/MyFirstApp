af64ebc7e6072d512371856b52416fae158e90f0
Aravindhan V <aravindhan.v@perfios.com>
2021-10-31 21:41:51 +0530

    ADDED: Greeting model

---
+++ src/main/java/com/bridgelabz/myfirstapp/model/Greeting.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.myfirstapp.model;
+
+public class Greeting {
+	private long count;
+	private String message;
+	
+	public Greeting(long count, String message) {
+		this.count = count;
+		this.message = message;s
+	}
+}
