41a6c4a0b60d646cf20708338e5ab4aaeb7917bd
Aravindhan V <aravindhan.v@perfios.com>
2021-10-31 19:06:56 +0530

    ADDED: handler for post request to /post

--- src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.bridgelabz.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,5 +27,8 @@ public class HelloWorldController {
 	return "Hello "+name+"!";
 	}
 	
-	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello "+user.getFirstName() + " " + user.getLastName();
+	}
 }
