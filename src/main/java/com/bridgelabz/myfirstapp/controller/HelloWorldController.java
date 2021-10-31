f7704127c406618b25f19f8617f741764266b8c7
Aravindhan V <aravindhan.v@perfios.com>
2021-10-31 19:24:57 +0530

    ADDED: User model

--- src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/myfirstapp/controller/HelloWorldController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.myfirstapp.model.User;
+
 @RestController
 @RequestMapping("/hellocontroller")
 public class HelloWorldController {
---
+++ src/main/java/com/bridgelabz/myfirstapp/model/User.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.myfirstapp.model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setFirstName() {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName() {
+		this.lastName = lastName;
+	}
+}
